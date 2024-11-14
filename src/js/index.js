5efeb4bccbbd21db1da69adb4ebb64011b2da26c
Michaelrick88 <michaelrick88@hotmail.com>
2024-11-14 23:56:17 +0000

    [michaelrick88 realice un contador en segundos]
    realicé un contador en segundos de 6 dígitos

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,32 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
+
+
+let seconds = 0;
+setInterval(() => {
+
+    const hundredThousands = Math.floor(seconds / 100000);
+    const tenThousands = Math.floor((seconds % 100000) / 10000);
+    const thousands = Math.floor((seconds % 10000) / 1000);
+    const hundreds = Math.floor((seconds % 1000) / 100);
+    const tens = Math.floor((seconds % 100) / 10);
+    const ones = seconds % 10;
+
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home
+        hundredThousands={hundredThousands}
+        tenThousands={tenThousands}
+        thousands={thousands}
+        hundreds={hundreds}
+        tens={tens}
+        ones={ones}
+    />);
+    seconds = seconds + 1;
+}, 10);
+
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+/* ReactDOM.createRoot(document.getElementById('app')).render(<Home/>); */
 
